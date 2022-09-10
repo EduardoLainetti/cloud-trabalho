6e6ca6864e866993cf72fb34cf982144adf7f316
Eduardo <eduardolainetti2@hotmail.com>
2022-09-10 10:28:57 -0300

    Teste code-deploy

--- routes/trabalho.js
+++ routes/trabalho.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express Teste CODE DEPLOY" });
+  res.send("trabalho final da disciplina de cloud");
 });
 
 module.exports = router;
