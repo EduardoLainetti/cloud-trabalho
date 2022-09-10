e898fe608b108664136b0c28720175a300219109
Eduardo <eduardolainetti2@hotmail.com>
2022-09-10 11:00:55 -0300

    Ajuste code deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express Teste CODE DEPLOY" });
+  res.render("index", {
+    title: "Trabalho disciplina Cloud",
+    text: "Teste beanstalk e code deploy",
+  });
 });
 
 module.exports = router;
